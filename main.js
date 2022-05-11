48d82a5a40cedbb45d6ce9f6d332af3c842a0393
Jackie Li <li.jackie565@gmail.com>
2022-05-11 01:17:33 -0400

    Finished BackEnd

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 
 
-const todos = [{
+let todos = [{
   title: 'Get groceries',
-  dueDate: '2021-20-3'
+  dueDate: '2021-20-3',
+  id: 'id1'
 }, {
   title: 'Wash car', 
-  dueDate: '2021-04-5'
+  dueDate: '2021-04-5',
+  id: 'id2'
 }, { 
   title: 'Make dinner',
-  dueDate: '2023-09-8'
+  dueDate: '2023-09-8',
+  id: 'id3'
 }];
 
 
@@ -24,10 +27,29 @@ function addTodo() {
   const datePicker = document.getElementById('date-input');
   const dueDate = datePicker.value;
 
+  //create an id
+  const id = '' + new Date().getTime();
+
   todos.push({
     title: title,
-    dueDate: dueDate
+    dueDate: dueDate,
+    id: id
   }); //push into array
+  display();
+}
+
+function deleteTodo(event) {
+  const deleteButton= event.target;
+  const idToDelete = deleteButton.id;
+
+  todos = todos.filter( (todo) => {
+    if (idToDelete === todo.id) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
   display();
 }
 
@@ -36,12 +58,19 @@ function display() {
   //reset the list
   document.getElementById('list').innerHTML = '';
 
-  todos.forEach( (todo) => {
-    const element = document.createElement('div');
-    const todoList = document.getElementById('list');
+  todos.forEach( function (todo) {
+    const element = document.createElement('div'); //creates a div
+    element.id = 'momeme';
+    const todoList = document.getElementById('list'); //for exsiting stuff
+    const deleteButton = document.createElement('button');
     //set the div
-    element.innerText = todo.title + ' ' + todo.dueDate;
-    todoList.appendChild(element);
+    element.innerText = todo.title + ' ' + todo.dueDate; //into div
+    todoList.appendChild(element);//into div
+
+    deleteButton.innerText = 'Delete';
+    deleteButton.onclick = deleteTodo; //dif compared to dom
+    deleteButton.id = todo.id //adds the id to the button
+    element.appendChild(deleteButton);//ionto div
   });
 }
 
